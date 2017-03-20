a0122a96b989b5159816d88f5cc168bfe322009d
RichVazz <r.vazz.c@gmail.com>
2017-03-19 18:51:16 -0600

    add basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,31 @@
  Rails.application.routes.draw do
  
-  get 'users/new'
+  #root url
+  root 'static_pages#home'
 
- root 'static_pages#home'
- 
+  #static pages
   get '/help', to: 'static_pages#help'
 
   get '/about', to: 'static_pages#about'
 
   get '/contact', to: 'static_pages#contact'
 
+  #users
   get '/signup', to: 'users#new'
 
   get '/users', to: 'users#show'
 
   post '/signup', to: 'users#create'
 
-
-
   resources :users
+
+  #login
+  get '/login', to: 'sessions#new'
+  
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
+
 
   
 end
