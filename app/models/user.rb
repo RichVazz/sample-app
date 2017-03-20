946ee06f04da3e0100cf7f43eae1da0cd40ec15c
RichVazz <r.vazz.c@gmail.com>
2017-03-20 01:48:47 -0600

    Add advance login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-
+	attr_accessor :remember_token
 	before_save { self.email = email.downcase }
 
 	validates :name, presence: true, length: {maximum: 50}
@@ -18,4 +18,27 @@ class User < ApplicationRecord
 	    BCrypt::Password.create(string, cost: cost)
 	  end
 
+	 # Returns a random token. used in advance login remember me
+	  def User.new_token
+	    SecureRandom.urlsafe_base64
+	  end
+
+	  # Remembers a user in the database for use in persistent sessions.
+	  def remember
+	    self.remember_token = User.new_token
+	    update_attribute(:remember_digest, User.digest(remember_token))
+	  end
+
+	  # Returns true if the given token matches the digest.
+	  def authenticated?(remember_token)
+	  	return false if remember_digest.nil?
+	    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	  end
+
+	  # Forgets a user in a persitent session
+	  def forget
+	    update_attribute(:remember_digest, nil)
+	  end
+
+	  
 end
